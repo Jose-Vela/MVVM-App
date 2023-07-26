cb0814b54b81933e44f3f1bc8495e119ae3e3f45
Jose-Vela <joseeloyvo@gmail.com>
2023-07-26 00:07:28 -0600

    Creando nuestro segundo test

--- app/src/test/java/com/example/mvvmapp/domain/GetQuotesUseCaseTest.kt
+++ app/src/test/java/com/example/mvvmapp/domain/GetQuotesUseCaseTest.kt
@@ -1,6 +1,7 @@
 package com.example.mvvmapp.domain
 
 import com.example.mvvmapp.data.QuoteRepository
+import com.example.mvvmapp.domain.model.Quote
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.coVerify
@@ -10,7 +11,7 @@ import org.junit.Before
 import org.junit.Test
 
 
-internal class GetQuotesUseCaseTest{
+class GetQuotesUseCaseTest{
 
     //@MockK      // Tendremos que configurar cada una de las respuestas que pudiera darnos esa clase mockeada.
     @RelaxedMockK       // Si no controlamos la respuesta de una de sus funciones el propio sistema nos dará una por defecto
@@ -26,13 +27,29 @@ internal class GetQuotesUseCaseTest{
 
     @Test
     fun `when the api doesnt return anything then get values from database`() = runBlocking {
-        // Given:
+        // Given
         coEvery { quoteRepository.getAllQuotesFromApi() } returns emptyList()
 
-        // When:
+        // When
         getQuotesUseCase()
 
         // Then
         coVerify(exactly = 1) { quoteRepository.getAllQuotesFromDatabase() }
+    }
+
+    @Test
+    fun `when the api return something then get values from api`() = runBlocking {
+        // Given
+        val myList = listOf(Quote("MI SEGUNDO CASO DE USO", "JOSÉ ELOY"))
+        coEvery { quoteRepository.getAllQuotesFromApi() } returns myList
+
+        // When
+        val response = getQuotesUseCase()
+
+        // Then
+        coVerify(exactly = 1) { quoteRepository.clearQuotes() }
+        coVerify(exactly = 1) { quoteRepository.insertQuotes(any()) }
+        coVerify(exactly = 0) { quoteRepository.getAllQuotesFromDatabase() }
+        assert(myList == response)
     }
 }
